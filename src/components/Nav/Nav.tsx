6e641fc74ba6b11ee35bb85916011f1ca3be0d4d
Carla Magalhães <carlamagalhes60@gmail.com>
2024-11-24 20:12:54 -0300

    Navlogo feita

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,12 @@
+import { NavLogo } from "components";
+
 export function Nav (){
     return (
-        <nav>
-            <p> Schedule <span>Mail</span></p>
+        <nav className="w-full h-20 bg-gray-50 border-gray-300">
+            <div className="w-full h-full max-w-7xl m-auto flex items-center">
+                <NavLogo/>
+                </div>
+            
         </nav>
 
     )
