7d21e764f8a94e375bebccee0c121bca0690b0eb
Carla Magalhães <carlamagalhes60@gmail.com>
2024-11-24 20:47:04 -0300

    botão finalizado

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,21 @@
-export function Button () {
+import classNames from "classnames";
+
+
+type Props = {
+    children: React.ReactNode
+    variant?: 'default' | 'dark' | 'primary' | 'light' 
+
+}
+
+export function Button ({variant, children} : Props) {
+
+    let bgColor = 'text-black'
+    if (variant === 'dark') bgColor = 'bg-primaryDark text white' 
+    if (variant === 'primary') bgColor = 'bg-primary hover:bg-primaryLight active:bg-primaryDark transition-all text-white';
+    if (variant === 'light') bgColor = 'bg-primaryLight text-white' 
     return (
-        <button>
-            
+        <button className={classNames('py-2 px-4 rounded-md font-bold', bgColor)}>
+            {children}
         </button>
     )
     
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,10 +1,12 @@
-import { NavLogo } from "components";
+import { Button, NavLogo } from "components";
 
 export function Nav (){
     return (
         <nav className="w-full h-20 bg-gray-50 border-gray-300">
-            <div className="w-full h-full max-w-7xl m-auto flex items-center">
+            <div className="w-full h-full max-w-7xl m-auto flex items-center justify-between">
                 <NavLogo/>
+                <Button variant="primary">Escrever agora</Button>
+                
                 </div>
             
         </nav>
