fe0643c53e783953c50bd073ba3b62fd8a3581a0
Carla Magalhães <carlamagalhes60@gmail.com>
2024-11-26 20:29:14 -0300

    traduzido calendário

--- src/components/Form/DateTimePicker.tsx
+++ src/components/Form/DateTimePicker.tsx
@@ -1,6 +1,9 @@
-import DatePicker from "react-datepicker";
+import DatePicker, {registerLocale} from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Controller, useFormContext } from "react-hook-form";
+import {ptBR} from 'date-fns/locale/pt-BR'
+registerLocale('pt-BR', ptBR)
+
 
 type Props = {
     name: string
@@ -12,10 +15,16 @@ export function DateTimePicker({ name }: Props) {
     return (
         <Controller
             name={name}
-            control={control} render={({ field: { onChange, value}}) => (<DatePicker
+            control={control} 
+            render={({ field: { onChange, value } }) =>
+                 (<DatePicker
+                 showTimeSelect
+                 locale="pt-BR"
+                 dateFormat="Pp"
+                
                 selected={new Date()}
                 onChange={(date: Date | null) => onChange(date)}
-            />)} 
+            />)}
         />
 
     );
